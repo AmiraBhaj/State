8730074a88c97b20740ef14bd7ff992c010c5c31
AmiraBhaj <belhaj.amira.gc@gmail.com>
2022-03-25 23:20:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      FullName: "Amira Belhaj",
+      Bio: "HAHHAHAAH",
+      imgSrc:
+        "https://femmedinfluence.fr/wp-content/uploads/2017/03/curly-hair-girl.jpg",
+      Profession: "Student",
+      Show: false,
+      count: 0,
+    };
+  }
+
+  handleClick = () => this.setState({ Show: !this.state.Show });
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState((prevState) => {
+        return {
+          count: prevState.count + 1,
+        };
+      });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleClick}>Toggle the person's photo</button>
+        <br />
+        {this.state.Show ? (
+          <img
+            src={this.state.imgSrc}
+            alt="femme metisse"
+            style={{ margin: 10 }}
+          />
+        ) : null}
+        <h1>The component has been rendered for {this.state.count} seconds</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
